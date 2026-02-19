051d7b991dc52cc2c1c546c729e82beca1d8f563
tomasmartinez <tomas.jmartinez@davinci.edu.ar>
2026-02-19 17:37:20 -0300

    mejoras

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'deep-bg': '#050708',      // Fondo Profundo
+				'action': '#191ba4',       // Acción/Principal
+				'accent': '#3cc2a2',       // Acentos/Hovers
+				'muted': '#5b82ad',        // Secundario/Apagado
+			},
+			fontFamily: {
+				'serif': ['"Lora"', 'serif'],       // Títulos y Citas
+				'sans': ['"Inter"', 'sans-serif'],  // Texto general
+			},
+		},
+	},
+	plugins: [],
+}
