1a2322606034ba7dfb4aa29c7bdfe00a36c467a7
Tomi <tomas.jmartinez@davinci.edu.ar>
2026-03-19 23:28:38 -0300

    mejoras y fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
 				'deep-bg': '#050708',      // Fondo Profundo
 				'action': '#191ba4',       // Acción/Principal
 				'accent': '#3cc2a2',       // Acentos/Hovers
-				'muted': '#5b82ad',        // Secundario/Apagado
+				'muted': '#5b82ad',        // Secundario — links y textos apagados
 			},
 			fontFamily: {
 				'serif': ['"Lora"', 'serif'],       // Títulos y Citas
